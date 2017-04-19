1d9d85859a9f8c6ab1ff608409717a8be6510bcd
Jose Juan Sandoval <juanchis_sandoval@hotmail.com>
2017-04-19 12:28:10 -0700

    XAM110 - Exercises 1 and 2
    -Exercise #1 is finished.
    -Working on Exercise #2.

---
+++ XAM110 - Intro to Cross Platform/Exercise 1/Solution/MyTunes.Shared/SongLoader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MyTunes
+{
+    public static class SongLoader
+	{
+		const string Filename = "songs.json";
+
+		public static async Task<IEnumerable<Song>> Load()
+		{
+            using (var reader = new StreamReader(await OpenData()))
+            {
+                return JsonConvert.DeserializeObject<List<Song>>(await reader.ReadToEndAsync());
+            }
+		}
+
+		private async static Task<Stream> OpenData()
+		{
+            #if WINDOWS_UWP || WINDOWS_PHONE_APP
+
+            var storageFile = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(Filename);
+
+            return await storageFile.OpenStreamForReadAsync();
+
+            #elif __ANDROID__
+
+            return Android.App.Application.Context.Assets.Open(Filename);
+             
+            #else
+            return null;
+            #endif
+        }
+    }
+}
+
---
+++ XAM110 - Intro to Cross Platform/Exercise 2/Solution/MyTunes.UWP/StreamLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using MyTunes.Shared;
+
+namespace MyTunes.UWP
+{
+    class StreamLoader : IStreamLoader
+    {
+        public Stream GetStreamForFilename(string filename)
+        {
+            return Windows.ApplicationModel.Package
+                .Current.InstalledLocation.GetFileAsync(filename)
+                .AsTask().Result.OpenStreamForReadAsync().Result;
+        }
+    }
+}
